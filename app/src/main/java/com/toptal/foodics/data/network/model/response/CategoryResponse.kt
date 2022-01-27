aae1e9453b9a06a5cf06a7507811846e856ff370
touhid <udoy.touhid@gmail.com>
2022-01-27 12:43:30 +0600

    Add network responses

---
+++ app/src/main/java/com/toptal/foodics/data/network/model/response/CategoryResponse.kt
@@ -0,0 +1,10 @@
+package com.toptal.foodics.data.network.model.response
+
+import com.toptal.foodics.data.network.model.Category
+
+/**
+ * Created by touhid on 24/Jan/2022.
+ */
+data class CategoryResponse(
+    val data : List<Category>
+)
---
+++ app/src/main/java/com/toptal/foodics/data/network/model/response/ProductResponse.kt
@@ -0,0 +1,10 @@
+package com.toptal.foodics.data.network.model.response
+
+import com.toptal.foodics.data.network.model.Product
+
+/**
+ * Created by touhid on 24/Jan/2022.
+ */
+data class ProductResponse(
+    val data: List<Product>
+)
